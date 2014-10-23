573175454d262742780cf6d91ffbdad598e9988c
cyberfox510 <cyberfox510@gmail.com>
2014-10-23 16:58:55 +0100

    Substituição do ciclo for por uma chamada à função Histograma, de forma a evitar duplicação de código

--- Entropia.m
+++ Entropia.m
@@ -3,21 +3,14 @@ function [H] = Entropia(P, A)
 %   P - fonte de informacao
 %   A - alfabeto de P
 
-%calcula-mos aqui tambem o numero de ocurrencias dos simbolos do alfabeto
-%na fonte de informacao
-ocorrencias = zeros(1, length(A)); %necessario para calcular probabilidades (podia ter usado funcao histograma mas como no enunciado parecia serem 2 exercicios separados fiz assim)
 probabilidades = zeros(1, length(A)); %inicializacao de matriz de probabilidades
 
-%n? de ocurrencias.
-%find return vector of linear indices and length the size of that vecto which equals the number of occurrences
-for (i = 1 : 1 : length(A))
-    ocorrencias(i) = length(find(P == A(i)));
-end
+hits = Histogram(P, A);
 
 length_P = length(P(:));
     
-for i=1:length(ocorrencias)
-    probabilidades(i)=ocorrencias(i)/length_P; %probabilidade 
+for i=1:length(hits)
+    probabilidades(i)=hits(i)/length_P; %probabilidade 
 end
 
 probs=find(probabilidades);% devolve vector com o indice linear de probs != 0
