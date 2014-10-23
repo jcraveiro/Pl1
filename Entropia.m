0a561c7dae1cfc0dc9258a2933f77a99934de9fc
cyberfox510 <cyberfox510@gmail.com>
2014-10-23 22:02:46 +0100

    Correcção dos argumentos de entrada da função 'Entropia'

--- Entropia.m
+++ Entropia.m
@@ -1,4 +1,4 @@
-function [H] = Entropia(P, A, hist)
+function [H] = Entropia(P, A)
 %Entropia devolve o limite minimo teorico para o numero medio de bits por simbolo.
 %   P - fonte de informacao
 %   A - alfabeto de P
@@ -6,6 +6,7 @@ function [H] = Entropia(P, A, hist)
 probabilidades = zeros(1, length(A)); %inicializacao de matriz de probabilidades
 
 length_P = length(P(:));
+hits = calcOcorrencias(P, A);
     
 for i=1:length(hits)
     probabilidades(i)=hist(i)/length_P; %probabilidade 
